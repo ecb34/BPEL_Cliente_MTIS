e77dae1cabb7b96cfab2114cba4004abc8ee3a40
ecb34 <ecb34@alu.ua.es>
2020-03-18 18:35:59 +0100

    added project

---
+++ BPEL_MTIS/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BPEL_MTIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.unidades = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.unidades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Referencia Producto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Unidades";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // unidades
+            // 
+            this.unidades.Location = new System.Drawing.Point(206, 197);
+            this.unidades.Name = "unidades";
+            this.unidades.Size = new System.Drawing.Size(120, 22);
+            this.unidades.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(106, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Comprar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(393, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.unidades);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Proceso de Compra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.unidades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown unidades;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BPEL_MTIS/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BPEL_MTIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var referencia = textBox1.Text;
+            var parseOk = int.TryParse(unidades.Text, out int unidadesResult);
+
+            if (string.IsNullOrEmpty(referencia) || !parseOk)
+            {
+                MessageBox.Show("Hay campos vacíos");
+            }
+            else
+            {
+                var request = new ProcesoCompra.ProcesoCompraRequest
+                {
+                    referenciaProducto = referencia,
+                    unidades = unidadesResult
+                };
+                var endpoint = new ProcesoCompra.ProcesoCompra
+                {
+                    Url = "http://localhost:8080/ode/processes/ProcesoCompra?wsdl"
+                };
+
+                var response = endpoint.process(request);
+
+                if (response.result)
+                {
+                    MessageBox.Show("Compra realizada correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("Error al realizar la compra");
+                }
+            }
+        }
+    }
+}
